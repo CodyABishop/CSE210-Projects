50e171758314448076ee80d6ae58ef244a1faebf
Cody Bishop <Codyabishop1215@gmail.com>
2023-09-20 17:39:11 -0600

    Prep 5 complete

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,36 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        StartUpMessage();
+
+        string userName=PromptUserName();
+        int userNumber = PromptUserNumber();
+        int numbersquared = SquareNumber(userNumber);
+        DisplayOutcome(userName, numbersquared);
+    }
+    static void StartUpMessage()
+    {
+        Console.WriteLine("Welcome!");
+    }
+    static string PromptUserName()
+    {
+        Console.Write("Please provide your User ID: ");
+        string name = Console.ReadLine();
+        return name;
+    }
+    static int PromptUserNumber()
+    {
+        Console.Write("Please provide a number: ");
+        int number = int.Parse(Console.ReadLine());
+        return number;
+    }
+    static int SquareNumber(int number)
+    {
+        int square= number * number;
+        return square;
+    }
+    static void DisplayOutcome(string name, int square)
+    {
+        Console.WriteLine($"{name}, your number squared is {square}");
     }
 }
