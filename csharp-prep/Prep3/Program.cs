0f341e253101a1d3548187006a25c2e6becba6f1
Cody Bishop <Codyabishop1215@gmail.com>
2023-09-20 16:22:56 -0600

    Finished Prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
-    }
+        String playAgain = "yes";
+        while ( playAgain == "yes" ){
+            Random randomGenerator = new Random();
+            int magicNumber = randomGenerator.Next(1, 101);
+
+            int guess = -1;
+
+            int guesses = 0;
+
+            while (guess != magicNumber)
+            {
+                Console.Write("What is the Mystic Numerical Entity? ");
+                guess = int.Parse(Console.ReadLine());
+
+                if (magicNumber > guess)
+                {
+                    Console.WriteLine("The Mystical Numerical Entity is higher!");
+                    guesses++;
+                }
+                else if (magicNumber < guess)
+                {
+                    Console.WriteLine("The Mystical Numerical Entity is lower!");
+                    guesses++;
+                }
+                else
+                {
+                    Console.WriteLine("You have guessed the Mystical Numerical Entity!");
+                    Console.WriteLine("Number of Guesses: " + guesses.ToString());
+                    Console.WriteLine("Would you like to play again? yes/no");
+                    playAgain = Console.ReadLine();
+                }
+            }
+        }
+    } 
 }
