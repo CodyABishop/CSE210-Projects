40f9a40d4ca7610a4423a0fc892a10ba947ca19c
Cody Bishop <Codyabishop1215@gmail.com>
2023-09-20 15:35:06 -0600

    Finished Prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,51 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage?");
+        string userInput = Console.ReadLine();
+        int score = int.Parse(userInput);
+        char letter;
+        string sign;
+        sign = "";
+        if (score >= 90){
+            letter = 'A';    
+        }
+        else if (score >= 80){
+            letter = 'B';    
+        }
+        else if (score >= 70){
+            letter = 'C';    
+        }
+        else if (score >= 60){
+            letter = 'D';    
+        }
+        else{
+            letter = 'F';    
+        }
+        if( (score % 10) >= 7  ){
+            sign = "+";
+        }
+        else if ( (score%10) <= 3 ){
+            sign = "-";
+        }
+        
+        if (letter == 'A'){
+            if (sign == "+"){
+                sign = "";
+            }
+        }
+        else if (letter == 'F'){
+            sign = "";
+        }
+            
+        if (score >= 70){
+            Console.Write("Congratulations, you have passed your course!");
+        }
+        else{
+            Console.Write("Unfortunately, you have not passed this course. Keep trying and you'll get it next time!");
+        }
+    
+        Console.Write("Your final grade was: " + letter + sign +  "!");
+
     }
 }
